162ea862e9ba053b2f3bcc15493321c5bd44d79b
KleitonCezarRodrigues <kleiton.cz@gmail.com>
2020-08-11 15:19:01 -0300

    Incluído template para inserção de eventos da agenda. Incluído gitignore para ignorar arquivos compilados.

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Evento(models.Model):
+    objects = None
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
@@ -14,4 +15,7 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M hrs')
 
